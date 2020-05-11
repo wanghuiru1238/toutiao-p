bdd7602746343f0d67ccf9d32e8d0e26b180de0a
wanghuiru123 <810462084@qq.com>
2020-05-11 22:10:26 +0800

    我的-头部用户信息

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="my-c">我的 页面</div>
+  <div class="my-c">
+    <van-cell-group>
+      <van-cell
+      center
+      title="单元格"
+      value="内容">
+          <van-image
+          width="50"
+          height="50"
+          slot="icon"
+          fit="cover"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <div slot="title">昵称</div>
+        <van-button
+        round
+        size="small">编辑资料</van-button>
+      </van-cell>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
