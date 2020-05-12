28bc9b15ddb933dcf535dfab4be9c71d747887c5
wanghuiru123 <810462084@qq.com>
2020-05-12 22:48:20 +0800

    我的-拦截器统一设置token

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,8 @@
 // 用户相关请求模块
 
 import request from '@/utils/request.js'
-// 在非组件模块中获取store(使用容器数据) 必须要加载store 跟在组件中的this.$store是一样的
-import store from '@/store/'
+// // 在非组件模块中获取store(使用容器数据) 必须要加载store 跟在组件中的this.$store是一样的
+// import store from '@/store/'
 // 登录注册请求接口
 export const login = (data) => {
   return request({
@@ -22,9 +22,9 @@ export const sendms = (mobile) => {
 export const getinfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    url: '/app/v1_0/user'
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }(封装token拦截器 所以此处可不设置请求携带token)
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,22 @@
 // 请求模块
 import axios from 'axios'
-
+// import { Store } from 'vuex'
+// 在非组件模块中获取store(使用容器数据) 必须要加载store 跟在组件中的this.$store是一样的
+import store from '@/store/'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
+})
+
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+  const { user } = store.state
+  // 如果用户已登录 给接口设置token
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
 })
 
 // 导出
