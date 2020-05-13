4a69c0040d6a574bceca87d2233a2469d957e63a
wanghuiru123 <810462084@qq.com>
2020-05-13 21:27:58 +0800

    展示频道文章列表

---
+++ src/api/article.js
@@ -0,0 +1,11 @@
+// 文章相关请求模块
+import request from '@/utils/request.js'
+
+// 获取文章列表
+export const getArticlesList = params => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_1/articles',
+    params
+  })
+}
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -4,14 +4,15 @@
       v-model="loading"
       :finished="finished"
       finished-text="没有更多了"
-      @load="onLoad"
+      @load="loadArticles"
     >
-       <van-cell v-for="item in list" :key="item" :title="item" />
+       <van-cell v-for="(article, index) in articles" :key="index" :title="article.title" />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getArticlesList } from '@/api/article'
 export default {
   name: 'ArticleList',
   components: {},
@@ -23,9 +24,10 @@ export default {
   },
   data () {
     return {
-      list: [],
-      loading: false,
-      finished: false
+      articles: [], // 文章列表
+      loading: false, // 控制加载中的loading状态
+      finished: false, // 控制加载结束状态,加载结束 不再触发加载更多
+      timestamp: null // 获取下一页的时间戳
     }
   },
   computed: {},
@@ -33,25 +35,38 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
+    async loadArticles () {
+      const res = await getArticlesList({
+        channel_id: this.channel.id, // 频道id
+        timestamp: this.timestamp || Date.now(), // 时间戳 请求什么时候的数据使用什么时候的时间戳
+        with_top: 1 // 是否包含文章置项(值为1时是包含,为0是不包含)
+      })
+      // console.log(res)
+      // 2将数据放到articles文章列表中
+      const { results } = res.data.data
+      this.articles.push(...results)
 
-        // 加载状态结束
-        this.loading = false
+      // 3设置本次加载状态结束
+      this.loading = false
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 1000)
+      // 4数据全部加载完成
+      if (results.length) {
+        this.timestamp = res.data.data.pre_timestamp
+      } else {
+        this.finished = true
+      }
     }
   }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.article-list {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 90px;
+  bottom: 50px;
+  overflow-y: auto;
+}
+</style>
