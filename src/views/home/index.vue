e08ef5b79520390c0913017246a0d64163d69b97
wanghuiru123 <810462084@qq.com>
2020-05-16 22:07:53 +0800

    频道编辑-获取登录与未登录的用户频道数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -53,6 +53,8 @@
 import ChannelEdit from './components/channel-edit'
 import { getchannels } from '@/api/user'
 import ArticleList from './components/article-list'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 export default {
   name: 'HomeIndex',
   components: {
@@ -67,7 +69,9 @@ export default {
       channels: [] // 频道列表
     }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
   created () {
     this.loadchannels()
@@ -75,9 +79,29 @@ export default {
   mounted () {},
   methods: {
     async loadchannels () {
-      const res = await getchannels()
-      // console.log(res)
-      this.channels = res.data.data.channels
+      let channels = []
+
+      if (this.user) {
+        // 已登录 请求获取线上的用户频道列表
+        const res = await getchannels()
+        channels = res.data.data.channels
+      } else {
+        // 没有登录 判断本地存储中是否有储存的频道列表
+        const locachannels = getItem('user-channels')
+
+        // 如果本地存储中有保存的频道列表就使用:
+        if (locachannels) {
+          channels = locachannels
+        } else {
+          // 未登录,本地存储也没有数据,就默认请求{推荐频道列表}
+          const res = await getchannels()
+          channels = res.data.data.channels
+        }
+      }
+      this.channels = channels
+      // const res = await getchannels()
+      // // console.log(res)
+      // this.channels = res.data.data.channels
     }
     // 可直接在模板中通过$event默认接收传递的参数
     // onUpdateActive (index) {
